4013779da88b2848635d82fb6001de1ad58e6e27
royster <1172201059@qq.com>
2023-05-20 21:07:41 +0800

    提交没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>vite + Ts</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '我爱你祖国'
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const fun = ()=> 
+{
+  console.log('test');
+}
+
+fun();
