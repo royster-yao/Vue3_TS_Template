1cafc49597c0415f6b4ae1724ad350521911da01
royster <1172201059@qq.com>
2023-06-22 20:03:26 +0800

    feat: main

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import 'element-plus/dist/index.css'
 // 配置element-plus国际化
 //@ts-expect-error忽略当前文件ts类型的检测否则有红色提示(打包会失败)
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 暗黑模式样式
+import 'element-plus/theme-chalk/dark/css-vars.css'
 // 引入模板的全局样式
 import '@/styles/index.scss'
 // svg 需要的配置代码
