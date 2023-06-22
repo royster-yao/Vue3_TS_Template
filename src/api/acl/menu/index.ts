6c79c7f56b5c63d07c4dd7cf6a1c3fcf13de0197
royster <1172201059@qq.com>
2023-06-22 20:03:52 +0800

    feat: 菜单组件

---
+++ src/api/acl/menu/index.ts
@@ -0,0 +1,37 @@
+import request from '@/utils/request'
+import type { MenuParams, PermissionResponseData } from './type'
+// 枚举地址
+enum API {
+  // 获取全部菜单与按钮的标识数据
+  ALLPERMISSION_URL = '/admin/acl/permission',
+  // 给某一级新增一个子菜单
+  ADDMENU_URL = '/admin/acl/permission/save',
+  // 更新某一个已有的菜单
+  UPDATE_URL = '/admin/acl/permission/update',
+  // 删除已有的菜单
+  DELETEMENU_URL = '/admin/acl/permission/remove/',
+}
+
+/**
+ * 获取菜单的数据
+ * @returns 返回菜单的数据
+ */
+export const reqAllPermission = () =>
+  request.get<any, PermissionResponseData>(API.ALLPERMISSION_URL)
+
+/**
+ * 添加与更新菜单的方法
+ * @param data 添加或更新的参数
+ * @returns 根据参数中是否存在id,从而返回不同的请求方法
+ */
+export const reqAddOrUpdateMenu = (data: MenuParams) => {
+  if (data.id) {
+    return request.put<any, any>(API.UPDATE_URL, data)
+  } else {
+    return request.post<any, any>(API.ADDMENU_URL, data)
+  }
+}
+
+// 删除某一个已有的菜单
+export const reqRemoveMenu = (id: number) =>
+  request.delete<any, any>(API.DELETEMENU_URL + id)
