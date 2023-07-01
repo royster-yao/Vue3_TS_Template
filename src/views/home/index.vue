03aa95f878b7d51522dcc27da2e637f199c01025
royster <1172201059@qq.com>
2023-07-01 12:31:59 +0800

    feat: 头像、logo组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,47 @@
 <template>
-  <div>
-    <h1>我是一次路由，登录成功后展示数据 {{ count }}</h1>
-    <el-button
-      style="margin-top: 10px"
-      type="primary"
-      size="big"
-      @click="count++"
-    >
-      start count
-    </el-button>
+  <el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="avatar" class="avatar" />
+      <div class="bottom">
+        <h3 class="title">{{ getTime() }}好{{ userStore.username }}</h3>
+        <p class="sub-title">姚的管理后台</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="svgIcon">
+    <svg-icon name="welcome" width="500px" height="400px"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const count = ref(0)
+import {} from 'vue'
+import useUserStore from '@/store/modules/user'
+import { getTime } from '@/utils/time'
+const userStore = useUserStore()
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.box {
+  display: flex;
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  .bottom {
+    margin-left: 20px;
+    .title {
+      font-size: 30px;
+      font-weight: 900;
+      margin-bottom: 30px;
+    }
+    .sub-title {
+      font-style: italic;
+      color: rgb(163, 167, 169);
+    }
+  }
+}
+.svgIcon {
+  display: flex;
+  justify-content: center;
+}
+</style>
