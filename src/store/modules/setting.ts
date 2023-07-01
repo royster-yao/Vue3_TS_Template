7bf2dbaa76c1eb740d85b4dea4b6e9f2e3b6c458
royster <1172201059@qq.com>
2023-07-01 12:27:42 +0800

    feat: 控制刷新、颜色的仓库

--- src/store/modules/setting.ts
+++ src/store/modules/setting.ts
@@ -1,12 +1,19 @@
 // 小仓库：layout组件相关的配置仓库
 import { defineStore } from 'pinia'
-
+import { SETITEM, GETITEM } from '@/utils/dark'
 const useLayoutSettingStore = defineStore('SettingStore', {
   state: () => {
     return {
       fold: false, // 用于菜单折叠还是收起的控制
       refsh: false, // 用于刷新效果
+      // 主题颜色切换
+      color: GETITEM('dark') ? GETITEM('dark') : 'rgba(255, 69, 0, 0.68)',
     }
+  },
+  actions: {
+    changeTheme() {
+      SETITEM('dark', this.color)
+    },
   },
 })
 
