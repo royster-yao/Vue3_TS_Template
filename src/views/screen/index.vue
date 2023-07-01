7852f54675c374cb9b4f4ce0afa33723e1d781d4
royster <1172201059@qq.com>
2023-07-01 12:32:34 +0800

    feat: 大屏组件

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,90 @@
 <template>
-  <h1>我是数据大屏一级路由组件</h1>
+  <div class="container">
+    <!-- 内容区 -->
+    <div class="screen" ref="screen">
+      <!-- 数据大屏的顶部 -->
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">
+          <Tourist class="tourist" />
+          <Sex class="sex" />
+          <Age class="age" />
+        </div>
+        <div class="center">中间</div>
+        <div class="right">右侧</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Top from './components/top/index.vue'
+import Tourist from './components/tourist/index.vue'
+import Sex from './components/sex/index.vue'
+import Age from './components/age/index.vue'
+// 获取数据大屏展示内容的DOM元素
+const screen = ref<any>()
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+// 定义大屏缩放的比例
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
+}
 
-<style scoped lang="scss"></style>
+// 监听视口的变化
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images//bg.png) no-repeat;
+  background-size: cover;
+  .screen {
+    position: fixed;
+    width: 1920px;
+    height: 1080px;
+    left: 50%;
+    top: 50%;
+    transform-origin: left top;
+    .top {
+      width: 100%;
+      height: 40px;
+    }
+    .bottom {
+      display: flex;
+      .right {
+        flex: 1;
+      }
+      .left {
+        flex: 1;
+        height: 1040px;
+        display: flex;
+        flex-direction: column;
+        .tourist {
+          flex: 1.2;
+        }
+        .sex {
+          flex: 1;
+        }
+        .age {
+          flex: 1;
+          background-color: pink;
+        }
+      }
+      .center {
+        flex: 2;
+      }
+    }
+  }
+}
+</style>
